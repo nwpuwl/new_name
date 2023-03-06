52edca1d3db1912024caab8539d07e2cecc2cdd5
nwpu_wl <1955353891@qq.com>
2023-03-06 15:26:45 +0800

    增加分词

--- hanzi/wuxing.py
+++ hanzi/wuxing.py
@@ -18,11 +18,29 @@ for line in raw_file_content:
     #print(strip_line_list)
     wuxing_dict[strip_line_list[0]] = strip_line_list[1]
 
-while True:
-    word = input('输入汉字:')
-    if word=='N' :
-        break
-    if word in  wuxing_dict:
-        print('五行是: ' + wuxing_dict[word])
-    else:
-        print("invalid words")
+
+guang_contet = open("guanguan.txt")
+words_list = []
+words_dic = {}
+for line in guang_contet:
+    strip_line = str.strip(line)
+    ss = list(strip_line)
+    if len(ss) == 0:
+        continue
+    for number in range(0, len(ss)):
+        end_number = min(number+3, len(ss))
+        if (ss[number] == '，' or ss[number] == '。') :
+            continue
+        for back in range(number+1, end_number):
+            if (ss[back] == '，' or ss[back] == '。'):
+                break
+            result = ss[number]+ss[back]
+            if result in words_dic:
+                continue
+            if ss[number] in wuxing_dict and ss[back] in wuxing_dict:
+                words_list.append(result)
+                words_dic[result] = wuxing_dict[ss[number]]+wuxing_dict[ss[back]]
+
+#print(words_list)
+print(words_dic)
+
